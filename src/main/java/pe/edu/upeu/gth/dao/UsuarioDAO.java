8e7fe7a6cb175d8dfdec430f136dbd6827aac1a5
Leandro Burgos <Leandro Burgos@UPEU>
2017-09-14 10:41:57 -0500

    CRUD

---
+++ src/main/java/pe/edu/upeu/gth/dao/UsuarioDAO.java
@@ -0,0 +1,56 @@
+package pe.edu.upeu.gth.dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import pe.edu.upeu.gth.config.AppConfig;
+import pe.edu.upeu.gth.interfaz.CRUDOperations;
+
+
+
+public class UsuarioDAO implements CRUDOperations {
+
+	String sql;
+    PreparedStatement ps;
+    CallableStatement cs;
+    ResultSet rs;
+    Connection cn;
+    DataSource d = AppConfig.getDataSource();
+
+    private JdbcTemplate jt;
+
+    public UsuarioDAO(DataSource dataSource) {
+        jt = new JdbcTemplate(dataSource);
+    }
+	
+	public ArrayList<Map<String, Object>> listar() {
+		sql = "SELECT * FROM RHTC_USUARIO";
+        return (ArrayList<Map<String, Object>>) jt.queryForList(sql);
+	}
+
+	public boolean add(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean edit(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean delete(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public ArrayList<Map<String, Object>> validar(String usuario, String pass) {
+        sql = "select * from rhvd_usuario where trim(no_usuario)=? and trim(pw_usuario)=?";
+        return (ArrayList<Map<String, Object>>) jt.queryForList(sql, usuario, pass);
+    }
+    
+}
