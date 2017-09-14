f2c02ccbd06f72db283ff3e88e82fcd562cf9009
Leandro Burgos <Leandro Burgos@UPEU>
2017-09-14 10:18:31 -0500

    globalProperties

---
+++ src/main/java/pe/edu/upeu/gth/properties/globalProperties.java
@@ -0,0 +1,23 @@
+package pe.edu.upeu.gth.properties;
+
+public class globalProperties {
+
+	public static String VERSION_CSS = "2.1.0";
+	public static String VERSION_JS = "2.7.0";
+
+	/* WebService Carga Academica */
+	public static final String keyApp = "4a7c88ee0791cad24a15d43a525982f8";
+	public static final String keyID = "d57d9c1cd0cfdec68805a5055388177b";
+	public static final String serverURI = "https://webapp.upeu.edu.pe/";
+	public static final String service = "https://webapp.upeu.edu.pe/webservices/wsdl4rrhh/";
+
+	public static final String DOCENTESXCURSO_METHOD[] = { "DocenteXCurso", "ns1", "ns1:DocenteXCursoResponse" };
+
+	/* oracle DB connection */
+	public static final String HOSTNAME = "192.168.21.9";
+	public static final String USER = "gth";
+	public static final String USER_PWD = "123";
+	public static final String PORT = "1521";
+	public static final String SID = "xe";
+
+}
