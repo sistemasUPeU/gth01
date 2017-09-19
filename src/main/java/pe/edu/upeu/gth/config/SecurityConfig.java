9125cf09f4a2eab5987be416197ee1ee777acfc3
Cesar <Cesar@AlphaDev3.lim.upeu.edu.pe>
2017-09-19 10:21:29 -0500

    hola

--- src/main/java/pe/edu/upeu/gth/config/SecurityConfig.java
+++ src/main/java/pe/edu/upeu/gth/config/SecurityConfig.java
@@ -17,6 +17,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/resources/**").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+				.loginPage("/login").permitAll().and().logout().permitAll().and().authorizeRequests().antMatchers("/valida").permitAll().anyRequest().anonymous();
 	}
 }
