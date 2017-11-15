610572c0c7eb522974f3f834a8c0413c6605d0f4
AlphaTeam-02-NicoleG <AlphaTeam-02-NicoleG@PCALPHA>
2017-11-15 11:33:10 -0500

    lalaa

--- src/main/webapp/resources/js/carpeta.js
+++ src/main/webapp/resources/js/carpeta.js
@@ -25,37 +25,11 @@
             });
         });
 
-$(document).ready(function(){
-            // Basic
-            $('.dropify').dropify();
 
-            // Translated
-            $('.dropify-fr').dropify({
-                messages: {
-                    default: 'Glissez-déposez un fichier ici ou cliquez',
-                    replace: 'Glissez-déposez un fichier ou cliquez pour remplacer',
-                    remove:  'Supprimer',
-                    error:   'Désolé, le fichier trop volumineux'
-                }
-            });
-
-            // Used events
-            var drEvent = $('.dropify-event').dropify();
-
-            drEvent.on('dropify.beforeClear', function(event, element){
-                return confirm("Do you really want to delete \"" + element.filename + "\" ?");
-            });
-
-            drEvent.on('dropify.afterClear', function(event, element){
-                alert('File deleted');
-            });
-<<<<<<< HEAD
-        });
-=======
             
-            $("#detalleR").hide();
-       
-        });
+//            $("#detalleR").hide();
+//       
+//        });
 
 function buscarDetalle(){	
 	dni = $("#dni").val();
@@ -85,4 +59,4 @@ function buscarDetalle(){
 
 }
 
->>>>>>> branch 'modulo-renuncias' of https://github.com/sistemasUPeU/gth01.git
+
