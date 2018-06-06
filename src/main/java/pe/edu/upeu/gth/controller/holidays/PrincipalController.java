9123e6f957f3b647ce1677d6eeb0432aef950e21
Jhorman Rus <jhormantito@upeu.edu.pe>
2018-06-06 10:13:44 -0500

    actual

--- src/main/java/pe/edu/upeu/gth/controller/holidays/PrincipalController.java
+++ src/main/java/pe/edu/upeu/gth/controller/holidays/PrincipalController.java
@@ -241,5 +241,25 @@ public class PrincipalController {
 	public ModelAndView reportes(HttpServletRequest request, HttpServletResponse response) {
 		return new ModelAndView("vacaciones/vac_reportes");
 	}
-
+	
+	@GetMapping("/historial")
+	public ModelAndView historial_tramite(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("vacaciones/historial_tramite");
+	}
+	
+	@RequestMapping(path = "/readFechaMod", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String leerFechaMod(HttpServletRequest RQ) {
+		String id = RQ.getParameter("id");
+		GestionarPrograVacacDAO DAO = new GestionarPrograVacacDAO(AppConfig.getDataSource());
+		return GSON.toJson(DAO.READFECHA(id));
+	}
+	
+	@RequestMapping(path = "/updateFechaMod", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String actualizarFechaMod(HttpServletRequest RQ) {
+		String id = RQ.getParameter("id");
+		String inicio = RQ.getParameter("inicio");
+		String fin = RQ.getParameter("fin");
+		GestionarPrograVacacDAO DAO = new GestionarPrograVacacDAO(AppConfig.getDataSource());
+		return GSON.toJson(DAO.UPDATEFECHA(id, inicio, fin));
+	}
 }
